272210b6efd23f5816d4855808e5ad6350e469e6
DESKTOP-OHBVMKI\admin <yuhiee123@gmail.com>
2022-09-28 16:29:41 +0900

    javascript 변수

---
+++ study01/study01.js
@@ -0,0 +1,10 @@
+var btn4 = document.getElementById("btn-click04");
+btn4.addEventListener("click", function () {
+    alert("네번째 버튼을 클릭");
+});
+
+// alert
+// confirm
+// prompt
+// document()
+// console.lot -> sout과 같음
